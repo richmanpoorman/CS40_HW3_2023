f81362a056ee8ac8aaf02b55296137365f35c55d
APrince22 <107816635+APrince22@users.noreply.github.com>
2023-10-06 22:20:41 -0400

    updated
    function contracts

--- locality/uarray2bTest.c
+++ locality/uarray2bTest.c
@@ -3,6 +3,59 @@
 #include <uarray.h>
 #include <assert.h>
 #include <stdio.h>
+#include <mem.h>
+#include <stdbool.h>
+
+void outofBounds() 
+{
+        int width = 0;
+        int height = 0;
+        int size = sizeof(int);
+        int blocksize = sizeof(int);
+        UArray2b_T array = UArray2b_new(width, height, size, blocksize);
+        int *elem = UArray2b_at(array, 5, 5);
+        printf("the elem is %i\n ", *elem);
+        UArray2b_free(&array);
+        assert(array == NULL);
+}
+
+void atBoundary() 
+{
+        int width = 5;
+        int height = 5;
+        int size = sizeof(int);
+        int blocksize = sizeof(int);
+        UArray2b_T array = UArray2b_new(width, height, size, blocksize);
+        int *elem = UArray2b_at(array, 5, 5);
+        printf("the elem is %i\n ", *elem);
+        UArray2b_free(&array);
+        assert(array == NULL);
+}
+
+void negativeAt() 
+{
+        int width = 5;
+        int height = 5;
+        int size = sizeof(int);
+        int blocksize = sizeof(int);
+        UArray2b_T array = UArray2b_new(width, height, size, blocksize);
+        int *elem = UArray2b_at(array, -2, -500);
+        printf("the elem is %i\n ", *elem);
+}
+
+void negativeDimensions()
+{
+        int width = -2;
+        int height = -2;
+        int size = sizeof(int);
+        int blocksize = sizeof(int);
+        UArray2b_T array = UArray2b_new(width, height, size, blocksize);
+        int *elem = UArray2b_at(array, 5, 5);
+        printf("the elem is %i\n ", *elem);  
+        UArray2b_free(&array);
+        assert(array == NULL);
+}
+
 void sizeTest() 
 {
         int width = 4;
@@ -178,7 +231,8 @@ void longLongTest()
 struct bigStruct {
         char new64kArray [64 * 1024];
 };
-void bigStructTest() {
+void bigStructTest() 
+{
         int width = 100;
         int height = 100;
         int size = sizeof(struct bigStruct);
@@ -194,8 +248,52 @@ void bigStructTest() {
         assert(testSize == size);
         assert(testBlocksize == 1);
 
+        struct bigStruct *testStruct = ALLOC(sizeof(*testStruct));
+        char *str = "This is a test";
+        for (int i = 0; i < 15; i++)
+                testStruct -> new64kArray[i] = str[i];
+        int col = 25;
+        int row = 30;
+        struct bigStruct *position = UArray2b_at(array, col, row);
+        *position = *testStruct;
+        
+        struct bigStruct *testGet = UArray2b_at(array, col, row);
+        for (int i = 0; i < 15; i++) {
+                assert(testGet -> new64kArray[i] == str[i]);
+        }
+        FREE(testStruct);
+        
+        UArray2b_free(&array);
+        assert(array == NULL);
         printf("Passed Big Struct Test\n");
 }
+
+struct superBigStruct {
+        long long new64kArray [64 * 1024];
+};
+
+void superBigStructTest() 
+{
+        int width = 100;
+        int height = 100;
+        int size = sizeof(struct superBigStruct);
+        UArray2b_T array = UArray2b_new_64K_block(width, height, size);
+
+        int testWidth = UArray2b_width(array);
+        int testHeight = UArray2b_height(array);
+        int testSize = UArray2b_size(array);
+        int testBlocksize = UArray2b_blocksize(array);
+
+        assert(testWidth == width);
+        assert(testHeight == height);
+        assert(testSize == size);
+        assert(testBlocksize == 1);
+
+        UArray2b_free(&array);
+        assert(array == NULL);
+        printf("Passed Super Big Struct Test\n");
+}
+
 int main(int argc, char *argv[]) 
 {
         sizeTest();
@@ -204,6 +302,11 @@ int main(int argc, char *argv[])
         new64kTest();
         longLongTest();
         bigStructTest();
+        superBigStructTest();
+        /* negativeDimensions(); */
+        /* negativeAt(); */
+        /* atBoundary(); */
+        /* outofBounds(); */
         (void) argc;
         (void) argv;
 }
