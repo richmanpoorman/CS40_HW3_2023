c1a170f05b6a49ee87224557605e03983830fa34
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-10-10 21:08:57 -0400

    bash updates

--- locality/runTests.bash
+++ locality/runTests.bash
@@ -20,23 +20,29 @@ TIMING_SUFFIX="Timing.txt"
 
 # imageTransform [imageName] [outputSuffix] [operation1] [operation2]
 imageTransform () {
-        jpegtran $3 $IMAGE_PATH$1.jpg | djpeg > $OUTPUT_PATH$1$2test.ppm
+        # jpegtran $3 $IMAGE_PATH$1.jpg | djpeg > $OUTPUT_PATH$1$2test.ppm
 
-        djpeg $IMAGE_PATH$1.jpg > $IMAGE_PATH$1.jpgToPpm
-        
-        valgrind --leak-check=full --show-leak-kinds=all ./ppmtrans \
-                $3 $4 $5 $IMAGE_PATH$1.jpgToPpm \
+        # djpeg $IMAGE_PATH$1.jpg > $IMAGE_PATH$1.jpgToPpm | \
+        # valgrind --leak-check=full --show-leak-kinds=all \
+        djpeg $IMAGE_PATH$1.jpg | \
+        ./ppmtrans \
+                $3 $4 $5 \
                 > $OUTPUT_PATH$1$2.ppm
-        
-        diffVal=(diff $OUTPUT_PATH$1$2test.ppm $OUTPUT_PATH$1$2.ppm)
-        if [ -z $diffVal ]; then
-                echo "$IMAGE_PATH$1$3$4$5 has a diff error"
-                echo $diffVal > $OUTPUT_PATH$1$2.diff
-        fi
 
-        rm $OUTPUT_PATH$1$2test.ppm
+        # diffVal="$(diff $OUTPUT_PATH$1$2test.ppm $OUTPUT_PATH$1$2.ppm)"
+        # if [ ! -z "$diffVal" ]; then
+        #         # display $OUTPUT_PATH$1$2test.ppm
+        #         # display $OUTPUT_PATH$1$2.ppm
+                
+        #         echo "$IMAGE_PATH$1$3$4$5 has a diff error"
+        #         echo $diffVal > $OUTPUT_PATH$1$2.diff
+        # else 
+        #         echo "Good Match"
+        # fi
+
+        # rm $OUTPUT_PATH$1$2test.ppm
         rm $OUTPUT_PATH$1$2.ppm
-        rm $IMAGE_PATH$1.jpgToPpm
+        # rm $IMAGE_PATH$1.jpgToPpm
         # ./ppmtrans $3 $4 $5 $IMAGE_PATH$1.ppm > \
         #                 $OUTPUT_PATH$1$2.ppm 
         # if [[ -f $IMAGE_PATH$1.jpg ]] && [[ "$#" -ge 3 ]]; then
@@ -67,9 +73,9 @@ for operation in "${TRANSFORMATIONS[@]}"; do
         
         op=($operation)
         for image in "${IMAGES[@]}"; do
-                echo "Image: $image.ppm"
+                echo "Image: $image"
 
-                echo $operation >> $OUTPUT_PATH$image$TIMING_SUFFIX
+                echo $operation >> "$OUTPUT_PATH$image$TIMING_SUFFIX"
                 imageTransform $image "-time${op[0]}${op[1]}" "$operation" \
                                      "-time $OUTPUT_PATH$image$TIMING_SUFFIX"
         done 
@@ -81,15 +87,16 @@ for operation in "${TRANSFORMATIONS[@]}"; do
         
         op=($operation)
         for image in "${IMAGES[@]}"; do
-                echo "Image: $image.ppm"
+                echo "Image: $image"
                 
                 for major in "${MAJORS[@]}"; do
-                        echo $operation >> \
-                                $OUTPUT_PATH$image$TIMING_SUFFIX$major
+                        echo $major
+                        echo "$major$operation" >> \
+                                "$OUTPUT_PATH$image$TIMING_SUFFIX$"
                         imageTransform $image "-time${op[0]}${op[1]}" \
                                 "$operation" \
                                 "-time $OUTPUT_PATH$image$TIMING_SUFFIX" \
-                                $major
+                                
                 done 
         done 
 done
@@ -100,7 +107,7 @@ imageTransform "ThisIsABadFile.ppm" "-badFileOutput"
 echo "No Flags Reading: "
 for operation in "${TRANSFORMATIONS[@]}"; do
         for image in "${IMAGES[@]}"; do
-                echo "Image: $image.ppm"
+                echo "Image: $image"
                 imageTransform $image "-noFlag${op[0]}${op[1]}" "$operation"
         done 
 done
